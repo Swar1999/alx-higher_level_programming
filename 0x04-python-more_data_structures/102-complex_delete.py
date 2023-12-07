ebad570e1896b8ef2b2110c34f7a9434b4923cde
swar1999 <muzan22kris@gmail.com>
2023-12-06 17:25:43 -0800

    task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,4 @@ def complex_delete(a_dictionary, value):
     for k, v in a_dictionary.items():
         if a_dictionary[k] == value:
             del a_dictionary[k]
-    return a_dectionary
+    return a_dictionary
