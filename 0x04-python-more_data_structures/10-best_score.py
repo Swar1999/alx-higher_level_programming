94705db0567f6baa7b1a7d39f0612e88b251bdc2
swar1999 <muzan22kris@gmail.com>
2023-12-06 16:46:32 -0800

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max_v = 0
+    max_k = None
+    for k, v in a_dictionary.items():
+        if v > max_v:
+            max_v = v
+            max_k = k
+    return max_v
